f1bd76fb9789cbf1dcfe99049f68240591147685
Panchanan Turuk <59464330+Panchananturuk1@users.noreply.github.com>
2021-01-20 15:50:21 +0530

    new change

--- index.php
+++ index.php
@@ -7,39 +7,45 @@
 	<meta name="viewport"
 		content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="mydesign.css" />
-
 	
 </head>
-<style>
-	
-		
+<style>	
 	
 	</style>
 <title> view event</title>
 <body class="bg" style="margin-left: 0px; margin-right: 0px; margin-top: 0px">
 
+
 	
 	 <form action="login2.php" method="post" enctype="multipart/form-data">
 	 
-	 
-	
-					<input type="submit" class="submit" type="submits"  name="submit"  style="font-size:x-large; margin-left:100px" >
-					<b> Try it free 7 says</b> then $20/mo. thereafter </input>
-		
-	 
-	<div class="box" style="margin-top:100px;" >
-				
 
-		<table style="margin-left:25%; margin-top:0px;">
+		
+		
+				<div style="width:420px; float:left; margin-top:10%;margin-left:10%">
+			<p style="font-size:60px; color:white"> Learn to code by<br />
+				watching others</p>
+			<p style="color:white">see how wxperienced developers solve problems in real-time.<br />
+				watching scripted tutorials is great, but understanding how<br />
+				developers think is invaluable</p>
+	
+			</div>
+			
+			<div style="width:500px;float:right;text-align:left">
+			
+				<button type="button" class="but2" name="button" >
+					<b> Try it free 7 says</b> then $20/mo. thereafter </button>
+			<div class="box" >	
+			<table style="margin-left:25%; margin-top:0px;">
 		
 		
 		<tr>
-			<td> <input type="text" id="Emailid" name="Emailid" placeholder="First Name" style="font-size:large" /> </td> 
+			<td> <input type="text" id="Emailid" name="First_Name" placeholder="First Name" style="font-size:large" /> </td> 
 		</tr>
 		
 		<tr>
 			<td>
-			 <input type="text" id="Emailid" name="Emailid" placeholder="Last Name" style="font-size:large" /> </td>
+			 <input type="text" id="Emailid" name="Last_Name" placeholder="Last Name" style="font-size:large" /> </td>
 		</tr>
 		
 		<tr>
@@ -55,11 +61,16 @@
 			<table style="margin-left:100px; top:20px" >
 			<tr style="margin-top:100px">
 				
-					<button type="submit" class="button" name="button" placeholder="Password"  style="font-size:x-large"> CLAIM YOUR FREE TRIAL </button> 
+					<button type="submit" class="but1" name="button" placeholder="Password"  style="font-size:x-large"> CLAIM YOUR FREE TRIAL </button> 
 				
 			</tr> </table>
 		</table>
-  </div>
+		</div>
+			</div>
+		
+			
+	
+  
   </form>
  
   
