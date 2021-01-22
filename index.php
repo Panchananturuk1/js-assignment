d71107ba5281a1a2e8b720a2c1624de22ab4a1b4
Panchanan Turuk <59464330+Panchananturuk1@users.noreply.github.com>
2021-01-22 16:48:24 +0530

    new css file

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 	<meta name="viewport"
 		content="width=device-width, initial-scale=1" />
 		
-	<link rel="stylesheet" type="text/css" href="NewDesign.css" />
+	<link rel="stylesheet" type="text/css" href="newstyle.css" />
 	
 	<style>
 @media screen and (max-width: 600px) {
